13ff29da349e2d5e07479a7abf2ee72f6504a159
Hélder Almeida <hcalmeida@uesc.br>
2024-04-01 19:23:01 -0300

    Code developed in 01.04.24 - First entity and configurations

---
+++ Domain/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace UescCoursesAPI.Domain;
+
+public class Course
+{
+   public int CourseId { get; set; }
+   public required string Name { get; set; }
+   public required string Status { get; set; }
+}
---
+++ Infrastructure/Persistence/UescCourseAPIContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UescCoursesAPI.Domain;
+
+namespace UescCoursesAPI.Infrastructure.Persistence;
+
+public class UescCourseAPIContext : DbContext
+{
+      public UescCourseAPIContext(DbContextOptions<UescCourseAPIContext> options) : base(options)
+      {
+         Database.EnsureCreated();
+      }
+   
+      public DbSet<Course> Courses { get; set; }
+
+      protected override void OnModelCreating(ModelBuilder modelBuilder)
+      {
+            modelBuilder.Entity<Course>().ToTable("Courses");
+            modelBuilder.Entity<Course>().HasKey(c => c.CourseId);
+      }
+
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      {
+            throw new System.NotImplementedException();
+      }
+}
